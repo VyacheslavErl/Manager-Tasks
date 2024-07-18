cbe2535c473fdc805a67ba001239456cda814ce9
VyacheslavErl <wwwolll153@gmail.com>
2024-07-18 20:13:31 +0400

    Finalized the list of priorities and worked on bugs

--- static/js/priorityList.js
+++ static/js/priorityList.js
@@ -1,62 +1,9 @@
+/* const priority = document.getElementById('priority');
 
-
-
-let Buttons = document.querySelectorAll("#priorityCircle")
-let WrapperButtons = document.querySelector("#page-title-priority")
-
-let activeButton = document.querySelector('.active');
-let inputClass = 'name'
-let  inputName  = document.querySelector(`.${inputClass}`);
-
-
-
-
-
-
-activeButton.addEventListener('mouseenter', ()=> {
-
-  WrapperButtons.classList.remove('page-title-priority-hide')
-  WrapperButtons.classList.add('page-title-priority-active')
-
-})
-WrapperButtons.addEventListener('mouseleave', ()=> {
-
-  WrapperButtons.classList.remove('page-title-priority-active')
-  WrapperButtons.classList.add('page-title-priority-hide')
-  })
-
-
-
-
-
-
-
-    Buttons.forEach(Button => Button.addEventListener('click', ()=> {
-
-
-
-        inputName.classList.remove(...[...inputName.classList].filter(n => n !== `${inputClass}`));
-
-if (Button.value !== 'priorityWhite') {
-  let className =   Button.getAttribute('priorityValue')
-    inputName.classList.add(`${className}`)
-}
-
-WrapperButtons.classList.remove('page-title-priority-active')
-WrapperButtons.classList.add('page-title-priority-hide')
-
-Button.parentNode.insertBefore(Button, activeButton);
-activeButton.classList.remove('active')
-Button.classList.add('active')
- 
-activeButton = document.querySelector('.active');   
-activeButton.addEventListener('mouseenter', ()=> {
-
-WrapperButtons.classList.remove('page-title-priority-hide')
-WrapperButtons.classList.add('page-title-priority-active')
-
-})
-
-
-
-    }))
+priority?.addEventListener('change', function() {
+  const colors = ["#FFFFFF", "#7091C2", "#70C273", "#C2A070", "#C27470"];
+  if (priority && priority.selectedIndex >= 0 && priority.selectedIndex < colors.length) {
+    priority.style.backgroundColor = colors[priority.selectedIndex];
+  }
+});
+ */
